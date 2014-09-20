124632c75da9168e53ca1c188b16167710f5f6c3
Aleksandr Tsertkov <tsertkov@gmail.com>
2014-09-21 01:43:47 +0200

    Major refactoring

--- test/resolve.js
+++ test/resolve.js
@@ -4,37 +4,64 @@ var
   testObj, testSelectors;
 
 testObj = {
-  key1: 'v1',
-  key2: 'v2',
-  key3: {
-    key31: 'v31',
-    key32: {
-      key321: 'v321',
-      key322: null
-    },
-    key33:[
-      1, 2, 3, "v3", null, "v5"
-    ]
+  k1: {
+    key: 'k1.key',
+    k11: { k1: { key: 'k1.k11.k1.key' } },
+    k12: [ [ { k121: 'k1.k12.0.0.k121' } ] ]
   },
-  key4: undefined,
-  key5: 5
+  k2: 'k2',
+  k3: 3,
+  k4: null,
+  k5: undefined,
+  k6: { k61: { k611: null } }
 };
 
 testSelectors = [
-  ['key1', 'v1'],
-  ['key1', '^', 'v1'],
-  ['key1', '^.', 'v1'],
-  ['key1', 'key1', 'v1'],
-  ['key1', 'key2', 'v1']
+  // absolute selectors - no startSelector given
+
+  ['k1', testObj.k1],
+  ['k2', 'k2'],
+  ['k3', 3],
+  ['k4', null],
+  ['k5', undefined],
+
+  ['k1.key', 'k1.key'],
+  ['k1.k12.0.0.k121', 'k1.k12.0.0.k121'],
+  ['k6.k61.k611', null],
+
+  ['missing', undefined],
+  ['k1.k12.0.missing', undefined],
+
+  // relative selectors with startSelector given
+
+  ['k2', 'missing', 'k2'],
+  ['k2', 'missing.key', 'k2'],
+  ['k1.k12.0.0.k121', 'k6.k61.k611', 'k1.k12.0.0.k121'],
+  ['k1.k12.0.0.k121', 'k6.k61.k611.missing.key', 'k1.k12.0.0.k121'],
+  ['k12.0.0.k121', 'k1', 'k1.k12.0.0.k121'],
+  ['k12.0.0.k121', 'k1.k12', 'k1.k12.0.0.k121'],
+  ['0.0.k121', 'k1.k12', 'k1.k12.0.0.k121'],
+  ['0.k121', 'k1.k12.0', 'k1.k12.0.0.k121'],
+
+  // same selector, different startPosition, different results
+
+  ['k1.key', 'k6.k61', 'k1.key'],
+  ['k1.key', 'k1.k11.k1', 'k1.k11.k1.key']
 ];
 
-assert(typeof resolve === 'function', 'module does not export a function');
+assert.strictEqual(typeof resolve, 'function', 'module does not export a function');
 
 testSelectors.forEach(function(testMeta){
   var
     selector = testMeta[0],
     startSelector = testMeta.length > 2 ? testMeta[1] : undefined,
-    value = testMeta[testMeta.length - 1];
+    expected = testMeta[testMeta.length - 1],
+    actual = resolve(testObj, selector, startSelector),
+    msg = [
+      'selector: "' + selector + '"',
+      'startSelector: "' + startSelector + '"',
+      'expected: "' + expected + '" !== actual: "' + actual + '"'
+    ].join(', ');
 
-  assert(resolve(testObj, selector, startSelector) === value);
+  assert.strictEqual(actual, expected, msg);
 });
